8e7b76255d719d27b4c04ea836bd23aac55da63f
ashah8055 <adshah8055@gmail.com>
2018-11-07 13:52:00 -0500

    Added Files for React JS Proj

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+const API_KEY = 'AIzaSyAXhMygivji6W7gF6k0C6XCq0WJnHQcxss';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('Ar Rehman');
+
+    }
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
